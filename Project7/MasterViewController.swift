dc7eb10e27d888fa8cc1409a5f30d0da01d476b0
joanne <jojoannexo@gmail.com>
2015-06-17 19:02:26 +0530

    show taglines of petitions

--- Project7/MasterViewController.swift
+++ Project7/MasterViewController.swift
@@ -10,17 +10,41 @@ import UIKit
 
 class MasterViewController: UITableViewController {
 
-    var objects = [AnyObject]()
+    var objects = [[String: String]]()
 
 
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
         
+        if let url = NSURL(string: urlString) {
+            if let data = NSData(contentsOfURL: url, options: .allZeros, error: nil) {
+                let json = JSON(data: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    // we're OK to parse!
+                    
+                    parseJSON(json) 
+                }
+            }
+        }
+    }
+    
+    func parseJSON(json: JSON) {
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            objects.append(obj)
+        }
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -55,7 +79,8 @@ class MasterViewController: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
 
         let object = objects[indexPath.row]
-        cell.textLabel!.text = object.description
+        cell.textLabel!.text = object["title"]
+        cell.detailTextLabel!.text = object["body"] 
         return cell
     }
 
